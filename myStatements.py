da36513646950422786ad96e3dea961b09d9c32c
RushabhNAKUM04 <156736670+RushabhNAKUM04@users.noreply.github.com>
2024-02-18 16:12:25 +0000

    adding file to repo

---
+++ myStatements.py
@@ -0,0 +1,8 @@
+my_list = ["apple","cherry","lemon"]
+my_dict = {"drink":"coffe" , "milk":"whole"}
+
+for item in my_list:
+    print(f"My favourite thing to eat: {item}")
+
+for _,value in my_dict.items():
+    print(f"My favourite tings to drink: {value}")
